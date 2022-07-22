3e420bdee0b81cfb8cc3800bb1d1a1fba9d170bf
lorenzof97 <67831959+lorenzof97@users.noreply.github.com>
2022-07-22 10:50:26 -0400

    Update leetcode9.js

--- leetcode9.js
+++ leetcode9.js
@@ -2,9 +2,10 @@ function isPalindrome(number) {
     let numString = number.toString();
     let string1, string2;
     numString.length % 2 !== 0 
+    // checking for an even or odd number of amount of characters in the given answer and slicing/cross-referencing each half thusly
     ? [string1, string2] 
     = [numString.slice(0, numString.length / 2), 
-      numString.slice(numString.length / 2 + 1, numString.length)]
+      numString.slice(numString.length / 2 + 1, numString.length)]    
     : [string1, string2] 
     = [numString.slice(0, numString.length / 2), 
       numString.slice(numString.length / 2, numString.length)]
@@ -12,4 +13,5 @@ function isPalindrome(number) {
     return check1 === check2;
 }
 
+// test condition
 console.log(isPalindrome(123454321));
